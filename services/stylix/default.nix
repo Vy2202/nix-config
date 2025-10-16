4254edeadd17623363333922ee21d8f2126f46d8
Vy <alaw2202@gmail.com>
2025-10-16 22:31:48 +0800

    修改配色为 darkmoss

--- services/stylix/default.nix
+++ services/stylix/default.nix
@@ -5,7 +5,7 @@
   ];
   stylix = {
     enable = true;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/atelier-savanna-light.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/darkmoss.yaml";
 
     targets.firefox.profileNames = [ "default" ];
   };
