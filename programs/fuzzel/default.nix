44f05485785f6506e3fdfe204313a29278090b10
Vy <alaw2202@gmail.com>
2025-10-17 08:45:00 +0800

    修改 fuzzel 字体大小，并禁用其图标

--- programs/fuzzel/default.nix
+++ programs/fuzzel/default.nix
@@ -1,9 +1,11 @@
-{ ... }:
+{ lib, ... }:
 {
   programs.fuzzel = {
     enable = true;
     settings = {
       main = {
+        font = lib.mkForce "Noto Sans:size=12";
+        icons-enabled = false;
         terminal = "ghostty";
         minimal-lines = true;
         line-height = 20;
