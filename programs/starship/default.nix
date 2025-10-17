f188f8ccb1ba04951ce8bbaffedc376161ae91dc
Vy <alaw2202@gmail.com>
2025-10-17 09:39:34 +0800

    更新 starship 的图标，使其能够完全显示 ⇡  和 ⇣ 。

--- programs/starship/default.nix
+++ programs/starship/default.nix
@@ -3,5 +3,11 @@
   programs.starship = {
     enable = true;
     enableBashIntegration = true;
+    settings = {
+      git_status = {
+        ahead = "⇡ ";
+        behind = "⇣ ";
+      };
+    };
   };
 }
