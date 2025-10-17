071400645132d943dc91c9e655e63ea1fe49f4a6
Vy <alaw2202@gmail.com>
2025-10-17 19:26:28 +0800

    将用户添加到 input 组

--- nixos/gray/default.nix
+++ nixos/gray/default.nix
@@ -41,6 +41,7 @@
       extraGroups = [
         "wheel"
         "video"
+        "input"
       ];
       hashedPassword = "$y$j9T$4QWBSIWxflEq9pReZOy2y.$RkOOXATQnfk2ypWt0PNh9/Q0uHaY3wleFvd.A/9h2eC";
       shell = pkgs.zsh;
