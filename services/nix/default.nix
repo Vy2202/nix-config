dcfcff08a40f4e105685e2a050e3fba594d73b42
Vy <alaw2202@gmail.com>
2025-10-27 14:07:30 +0000

    为 nix 添加 sshServer 功能

--- services/nix/default.nix
+++ services/nix/default.nix
@@ -25,10 +25,19 @@
       "wei"
     ];
   };
+
   nix.gc = {
     automatic = true;
     dates = "weekly";
     options = "--delete-older-than 7d";
+  };
+
+  nix.sshServe = {
+    enable = true;
+    keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMOgsldLKlYm6xLoLFPlfEgEKH3OoE0ubyVUyZz3Pmmz wei@gray"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIr9JuIDA/YHCbnniAvRePHHVhMeZiubvaWn6WyNKp5J wei@blue"
+    ];
   };
 
   nixpkgs.config.allowUnfree = true;
