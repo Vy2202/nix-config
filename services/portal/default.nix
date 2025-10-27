ab8ba55358b45cd3042636bbac1008d9fc144296
Vy <alaw2202@gmail.com>
2025-10-27 14:01:18 +0800

    修复 portal 无法正常启动的问题

--- services/portal/default.nix
+++ services/portal/default.nix
@@ -1,6 +1,15 @@
-{ ... }:
+{ pkgs, ... }:
 {
   xdg.portal = {
     enable = true;
+    extraPortals = [
+      pkgs.xdg-desktop-portal-gtk
+      pkgs.xdg-desktop-portal
+    ];
+  };
+
+  systemd.user.services.xdg-desktop-portal-gtk.environment = {
+    WAYLAND_DISPLAY = "wayland-1";
+    XDG_CURRENT_DESKTOP = "niri";
   };
 }
