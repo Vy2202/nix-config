349b4363e14d890dd1a9b3bc8f0cbcc23cf2025f
Vy <alaw2202@gmail.com>
2025-12-13 06:45:38 +0800

    使用 flake 管理 flake 的开发环境

--- flake.nix
+++ flake.nix
@@ -1,17 +1,34 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
-    { nixpkgs, ... }:
-    {
-      nixosConfigurations."gray" = nixpkgs.lib.nixosSystem {
-        modules = [ ./nixos/gray ];
-      };
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        nixosConfigurations = {
+          "gray" = nixpkgs.lib.nixosSystem {
+            modules = [ ./nixos/gray ];
+          };
 
-      nixosConfigurations."blue" = nixpkgs.lib.nixosSystem {
-        modules = [ ./nixos/blue ];
-      };
-    };
+          "blue" = nixpkgs.lib.nixosSystem {
+            modules = [ ./nixos/blue ];
+          };
+        };
+
+        devShells.default =
+          with pkgs;
+          mkShell {
+            packages = [
+              nil
+            ];
+          };
+      }
+    );
 }
