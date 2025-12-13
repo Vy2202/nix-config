08b98bd9a0199ff5044e56050f9ff7f0cfed6544
Vy <alaw2202@gmail.com>
2025-12-13 15:20:04 +0800

    使用固定 IP

--- nixos/blue/hardware.nix
+++ nixos/blue/hardware.nix
@@ -58,11 +58,19 @@
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
+  # networking.useDHCP = lib.mkDefault true;
   # networking.interfaces.eno1.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp1s0.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp2s0.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp4s0.useDHCP = lib.mkDefault true;
+  networking.interfaces.enp1s0 = {
+    ipv4.addresses = [
+      {
+        address = "192.168.0.100";
+        prefixLength = 24;
+      }
+    ];
+  };
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
